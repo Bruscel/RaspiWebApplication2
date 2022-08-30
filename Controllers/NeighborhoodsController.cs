a8ec40c2485d60346a2ee11a4c3c56239a7d2991
Myron <bruscel96@gmail.com>
2022-08-30 11:11:13 -0400

    Updates to neighborhood view to display all neighborhoods.

--- Controllers/NeighborhoodsController.cs
+++ Controllers/NeighborhoodsController.cs
@@ -8,13 +8,18 @@ using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using Microsoft.AspNetCore.Http;
 using RaspiWebApplication2.Models.Dto;
+using System.IO;
 
 namespace RaspiWebApplication2.Controllers
 {
     public class NeighborhoodsController : Controller
     {
+        //nodeTestData Model conection 
+        List<Hoods> neighborhoods = new List<Hoods>();
+
         //********mySql Server connection***********
         MySqlCommand com = new MySqlCommand();
+        MySqlDataReader dr;
         MySqlConnection con = new MySqlConnection();
 
         private readonly ILogger<NeighborhoodsController> _logger;
@@ -38,13 +43,13 @@ namespace RaspiWebApplication2.Controllers
             com.Connection = con;
             com.CommandText = "CALL ADD_NEIGHBORHOOD(@Neighborhood_name, @City, @Zip, @State, @Latitude, @Longitude, @Feet_radius, @Insertion_use)";
 
-            com.Parameters.Add("@Neighborhood_name", MySqlDbType.String, 25).Value = h.hood_name;
+            com.Parameters.Add("@Neighborhood_name", MySqlDbType.String, 25).Value = h.neighborhood_name;
             com.Parameters.Add("@City", MySqlDbType.String, 8).Value = h.city;
             com.Parameters.Add("@Zip", MySqlDbType.String, 12).Value = h.zip;
             com.Parameters.Add("@State", MySqlDbType.String, 35).Value = h.state;
             com.Parameters.Add("@Latitude", MySqlDbType.String, 8).Value = h.latitude;
             com.Parameters.Add("@Longitude", MySqlDbType.String, 8).Value = h.longitude;
-            com.Parameters.Add("@Feet_radius", MySqlDbType.String, 8).Value = h.radius;
+            com.Parameters.Add("@Feet_radius", MySqlDbType.String, 8).Value = h.feet_radius;
             com.Parameters.Add("@Insertion_use", MySqlDbType.String, 8).Value = "User1";
 
 
@@ -52,6 +57,59 @@ namespace RaspiWebApplication2.Controllers
             com.ExecuteNonQuery();
             con.Close();
             return RedirectToAction("getNeighborhoods");
+        }
+
+
+        public void fileLoader(object sender, EventArgs e)
+        {
+            
+
+        }
+        public IActionResult FetchData()
+        {
+            if (neighborhoods.Count > 0)
+            {
+                neighborhoods.Clear();
+
+            }
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "select * from neighborhoods";
+
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    neighborhoods.Add(new Hoods()
+                    {
+                        neighborhood_name = dr["neighborhood_name"].ToString()
+                        ,
+                        latitude = dr["latitude"].ToString()
+                        ,
+                        longitude = dr["longitude"].ToString()
+                        ,
+                        city = dr["city"].ToString()
+                        ,
+                        state = dr["state"].ToString()
+                        ,
+                        zip = dr["zip"].ToString()
+                        ,
+                        feet_radius = dr["feet_radius"].ToString()
+                    });
+                }
+                con.Close();
+
+                return new JsonResult(new { neighborhoods })
+                {
+                    StatusCode = StatusCodes.Status200OK // Status code here 
+                };
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         /*
--- Models/Dto/Hoods.cs
+++ Models/Dto/Hoods.cs
@@ -7,12 +7,12 @@ namespace RaspiWebApplication2.Models.Dto
 {
     public class Hoods
     {
-        public string hood_name { get; set; }
+        public string neighborhood_name { get; set; }
         public string latitude { get; set; }
         public string longitude { get; set; }
         public string city { get; set; }
         public string state { get; set; }
         public string zip { get; set; }
-        public string radius { get; set; }
+        public string feet_radius { get; set; }
     }
 }
